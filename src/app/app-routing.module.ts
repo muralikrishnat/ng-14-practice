a1f9ebd2b3594d3ec0cb02956806086a7e55f959
Muralikrishna T <muralikrishnat@TECH-LP-1399.local>
2022-09-19 10:44:52 -0400

    Added routing and basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './comps/home/home.component';
+import { InventoryComponent } from './comps/inventory/inventory.component';
+import { LoginComponent } from './comps/login/login.component';
+import { RegisterComponent } from './comps/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'inventory', component: InventoryComponent
+  },
+  {
+    path: '', component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
